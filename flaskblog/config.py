d3ae39c9b2c8b78776f09aba1284a2f9c6d9921a
Kaushik Dey <kaudey@publicisgroupe.net>
2024-08-05 17:49:16 +0530

    [fix]: Fixed the config

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,12 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def custom_getenv(key, default):
+    value = os.getenv(key)
+    if value is None:
+        return default
+    return value
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')   
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    SECRET_KEY = custom_getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = custom_getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///default.db')
+    MAIL_SERVER = custom_getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(custom_getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = custom_getenv('MAIL_USE_TLS', 'True').lower() in ['true', '1', 't']
+    MAIL_USE_SSL = custom_getenv('MAIL_USE_SSL', 'False').lower() in ['true', '1', 't']
+    MAIL_USERNAME = custom_getenv('EMAIL_USER', 'default_email_user')
+    MAIL_PASSWORD = custom_getenv('EMAIL_PASS', 'default_email_pass')
